ba1dacb4af830566442b53a9713fe9a668832071
Nilin <itsmenilin18@gmail.com>
2025-01-08 20:29:00 +0530

    Add a logout function

--- controllers/authController.js
+++ controllers/authController.js
@@ -77,3 +77,18 @@ export const login = async (req, res) => {
     });
   }
 };
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      samesite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+    return res.status(200).json({ message: "successfully logged out" });
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+};
